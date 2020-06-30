a43da1620059c29c7cbb79a6722f0f570507cc5f
Marat Gumerov <gumerovm74@gmail.com>
2020-06-30 12:16:02 +0300

    Глобальные объекты

---
+++ index.js
@@ -0,0 +1,8 @@
+const userObj = require('./user')
+console.log(userObj);
+console.log('Node index для запуска');
+
+console.log(obj.user);
+
+obj.sayHello()
+
---
+++ user.js
@@ -0,0 +1,17 @@
+const user = {
+  name: 'Elena',
+  age: 25
+}
+
+const user2 = {
+  name: 'Igor'
+}
+
+
+module.exports = {
+  user:user,
+  sayHello() {
+    console.log('Hello');
+
+  }
+}
