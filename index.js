ca5ff58b2c6e2f392b871377ed22279e47f7b135
Marat Gumerov <gumerovm74@gmail.com>
2020-07-05 19:00:38 +0300

    Connect html simple pages

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require ('express')
-
+const path = require('path')
 //(1)Создание сервера
 const app = express()
+
+/*(1)
+ Гет запрос
+*/
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'about.html'))
+})
+
+
+
+
+
+
+
+
+
 
 // создаем Порт - занный вручную или по умолчанию 3000
 const PORT = process.env.PORT || 3000
