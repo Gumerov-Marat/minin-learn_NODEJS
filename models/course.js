5c1a5bee50eb52443b3093e3bf5f8c32144a1d4c
Marat Gumerov <gumerovm74@gmail.com>
2020-07-09 13:09:52 +0300

    refactor

--- models/course.js
+++ models/course.js
@@ -1,26 +1,47 @@
-const uuid = require('uuid/v4')
+const { v4: uuidv4 } = require('uuid')
 const fs = require('fs')
 const path = require('path')
-
 
 class Course {
   constructor(title, price, img) {
     this.title = title
     this.price = price
     this.img = img
-    this.id = uuid()
+    this.id = uuidv4()
+  }
+
+  toJSON() {
+    return {
+      title: this.title,
+      price: this.price,
+      img: this.img,
+      id: this.id
+    }
   }
 
   async save() {
     const courses = await Course.getAll()
+    courses.push(this.toJSON())
 
-    console.log('Courses', courses );
+    return new Promise((resolve, reject) => {
+      fs.writeFile(
+        path.join(__dirname, '..', 'data', 'courses.json'),
+        JSON.stringify(courses),
+        (err) => {
+          if (err) {
+            reject(err)
+          } else {
+            resolve()
+          }
+        }
+      )
+    })
   }
 
-  static getAll(){
+  static getAll() {
     return new Promise((resolve, reject) => {
       fs.readFile(
-        path.join(__dirname, '../data/courses.json'),
+        path.join(__dirname, '..', 'data', 'courses.json'),
         'utf-8',
         (err, content) => {
           if (err) {
@@ -33,3 +54,5 @@ class Course {
     })
   }
 }
+
+module.exports = Course
